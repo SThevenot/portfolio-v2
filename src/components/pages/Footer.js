e430563f4379442d7d4be79d1501ff868135eb17
SThevenot <sktmh007@gmail.com>
2022-10-10 08:39:57 -0500

    fixed social link icons in footer

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,37 +1,39 @@
+/** @format */
+
 import React from "react";
-import "../../styles/Footer.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fa-brands fa-discord } from "@f@fortawesome/free-solid-svg-icons"
+import "../../styles/Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
-    return (
-<footer>
-<div>
-  <button>
-    <a href="https://github.com/SThevenot" target="_blank"
-      ><i class="fa-brands fa-github"></i
-    ></a>
-  </button>
-  <button>
-    <a
-      href="https://discordapp.com/users/MoreThanAwkward456#0144"
-      target="_blank"
-      ><i class="fa-brands fa-discord"></i
-    ></a>
-  </button>
-  <button>
-    <a
-      href="https://open.spotify.com/user/connfetti007?si=b4e9d125354c4ec2"
-      target="_blank"
-      ><i class="fa-brands fa-spotify"></i
-    ></a>
-  </button>
-  <button>
-    <a href="mailto:sktmh007@gmail.com"
-      ><i class="fa-solid fa-envelope"></i
-    ></a>
-  </button>
-</div>
-</footer>
-);
+  return (
+    <footer>
+      <div>
+        <button>
+          <a href="https://github.com/SThevenot" target="_blank">
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+          </a>
+        </button>
+        <button>
+          <a
+            href="https://discordapp.com/users/MoreThanAwkward456#0144"
+          >
+            <FontAwesomeIcon icon="fa-brands fa-discord" />
+          </a>
+        </button>
+        <button>
+          <a
+            href="https://open.spotify.com/user/connfetti007?si=b4e9d125354c4ec2"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon="fa-brands fa-spotify" />
+          </a>
+        </button>
+        <button>
+          <a href="mailto:sktmh007@gmail.com">
+            <FontAwesomeIcon icon="fa-solid fa-envelope" />
+          </a>
+        </button>
+      </div>
+    </footer>
+  );
 }
