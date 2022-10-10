233a245746a9b2810a302d7f0fafd9248ab7f64c
SThevenot <sktmh007@gmail.com>
2022-10-10 10:05:01 -0500

    fixed icons and refactored education section for react

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React from "react";
 import NavTabs from "./components/NavTabs";
 import Home from "./components/pages/Home";
 import Footer from "./components/pages/Footer";
+import Education from "./components/pages/Education"
+import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
   return (
@@ -12,6 +14,9 @@ function App() {
       <section id="about-me">
         <Home />
       </section>
+      <section id="education">
+    <Education/>
+    </section>
       <Footer />
     </>
   );
--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -2,35 +2,35 @@
 
 import React from "react";
 import "../../styles/Footer.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaGithub } from "react-icons/fa";
+import { FaDiscord } from "react-icons/fa";
+import { FaSpotify } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer>
       <div>
         <button>
-          <a href="https://github.com/SThevenot" target="_blank">
-            <FontAwesomeIcon icon="fa-brands fa-github" />
+        <a href="https://github.com/SThevenot">
+          <FaGithub />
+        </a>
+        </button>
+        <button>
+          <a href="https://discordapp.com/users/MoreThanAwkward456#0144">
+            <i>
+              <FaDiscord />
+            </i>
           </a>
         </button>
         <button>
-          <a
-            href="https://discordapp.com/users/MoreThanAwkward456#0144"
-          >
-            <FontAwesomeIcon icon="fa-brands fa-discord" />
-          </a>
-        </button>
-        <button>
-          <a
-            href="https://open.spotify.com/user/connfetti007?si=b4e9d125354c4ec2"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon="fa-brands fa-spotify" />
+          <a href="https://open.spotify.com/user/connfetti007?si=b4e9d125354c4ec2">
+            <FaSpotify />
           </a>
         </button>
         <button>
           <a href="mailto:sktmh007@gmail.com">
-            <FontAwesomeIcon icon="fa-solid fa-envelope" />
+            <FaEnvelope />
           </a>
         </button>
       </div>
