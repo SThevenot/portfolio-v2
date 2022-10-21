f3ced720b1e5490e7704d33f7461765a865f9223
SThevenot <sktmh007@gmail.com>
2022-10-21 15:56:24 -0500

    conditional rendered navbar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,32 +1,85 @@
 /** @format */
 import { NavLink } from "react-router-dom";
-import React, { useState } from "react";
+import React from "react";
 import "../styles/Navbar.css";
+import { useState, useEffect } from "react";
 import { bubble as Menu } from "react-burger-menu";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-export default (props) => {
-  return (
-    <nav>
-      <h1 id="title"><NavLink to="/">Sarah Thevenot</NavLink></h1>
-      <Menu {...props} right>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/themetoggler">Theme Toggler</NavLink>
-          </li>
-          <li>
-            <NavLink to="/education/resume">Resume/Education</NavLink>
-          </li>
-          <li>
-            <NavLink to="/projects">My Work</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact Me</NavLink>
-          </li>
-        </ul>
-      </Menu>
-    </nav>
-  );
-};
+class NavTabs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDesktop: false,
+    };
+
+    this.updatePredicate = this.updatePredicate.bind(this);
+  }
+  componentDidMount() {
+    this.updatePredicate();
+    window.addEventListener("resize", this.updatePredicate);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updatePredicate);
+  }
+
+  updatePredicate() {
+    this.setState({ isDesktop: window.innerWidth > 767 });
+  }
+
+  render() {
+    const isDesktop = this.state.isDesktop;
+
+    return (
+      <nav>
+        {isDesktop ? (
+          <>
+            <Navbar>
+              <Container>
+                <Navbar.Brand id="title"><NavLink to="/">Sarah Thevenot</NavLink></Navbar.Brand>
+                <Nav className="me-auto">
+                  <ul>
+                  <NavLink to="/themetoggler" className="links">Theme</NavLink>
+                  <NavLink to="/resume/education" className="links">Resume</NavLink>
+                  <NavLink to="/projects" className="links">Projects</NavLink>
+                  <NavLink to="/contact" className="links">Contact Me</NavLink>
+                  </ul>
+                </Nav>
+              </Container>
+            </Navbar>
+          </>
+        ) : (
+          <>
+            <h1 id="title">
+              <NavLink to="/">Sarah Thevenot</NavLink>
+            </h1>
+            <Menu right>
+              <ul>
+                <li>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/themetoggler">Theme Toggler</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/education/resume">Resume/Education</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/projects">My Work</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact">Contact Me</NavLink>
+                </li>
+              </ul>
+            </Menu>
+          </>
+        )}
+      </nav>
+    );
+  }
+}
+
+export default NavTabs;
