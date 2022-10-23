212fd389b5e9a8f4c207cb62a1b06cad75748aac
SThevenot <sktmh007@gmail.com>
2022-10-23 11:29:13 -0500

    changed background images and added text animation on home page

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -3,7 +3,7 @@
 import React from "react";
 import "../../styles/home.css";
 import sarah from "../../images/sarah.jpeg";
-
+import { TypeAnimation } from 'react-type-animation';
 
 
 export default function Home() {
@@ -11,21 +11,29 @@ export default function Home() {
     <div className="container-fluid" id="aboutMe">
       <div className="col-3 text-center">
         <h1 id="aboutH1" className="mb-4">
-          About Me
+          About Me...
         </h1>
         <img src={sarah} alt="Sarah" className="sarahImg" />
       </div>
-      <div className="col-8">
-        <p id="infoText">
-          My name is Sarah Thevenot and I am currently enrolled in University of
-          Denver's Coding Bootcamp. I graduated high school in 2020 and am
-          recently unemployed trying to pursue my education and start a career
-          as a web developer. I really enjoy building and designing websites and
-          can't wait to keep learning and growing throughout this course. I
-          previously was an employee at Jersey Mike's subs and worked my way up
-          to assistant manager before I turned 19. I look forward to starting my
-          career and making the most of my life!
-        </p>
+      <div className="col-8 animationText">
+      <TypeAnimation
+    // Same String at the start will only be typed once, initially
+    sequence={[
+    'I am a junior developer...',
+    1000,
+    'I am front-end focused...',
+    1000,
+    'I am a full-stack bootcamp graduate...',
+    1000,
+    'I am a dedicated coder...',
+    1000,
+    ]}
+    speed={30} // Custom Speed from 1-99 - Default Speed: 40
+    className="animation"
+    wrapper="span" // Animation will be rendered as a <span>
+    repeat={Infinity} // Repeat this Animation Sequence infinitely
+  />
+
       </div>
     </div>
   );
