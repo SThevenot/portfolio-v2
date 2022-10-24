df85d99932ccb1a9b4b7ec0bae02eef1827e37e1
SThevenot <sktmh007@gmail.com>
2022-10-24 15:29:38 -0500

    re-formatted resume section

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,7 +3,7 @@
 import React from "react";
 import "../../styles/Resume.css";
 import myResume from "../../files/sarah-thevenot-resume.pdf";
-import { Table } from "react-bootstrap";
+import { Table, Row, Col } from "react-bootstrap";
 
 export default function Resume() {
   return (
@@ -16,89 +16,98 @@ export default function Resume() {
           </a>
         </button>
       </div>
-      <div id="education">
-        <dl>
-          <h1>Education</h1>
-          <dt>Rochester High School</dt>
-          <dd>2016-2020</dd>
-          <br />
-          <dt>Lincoln Land Community College</dt>
-          <dd>2018-2021</dd>
-          <br />
-          <dt>University of Denver Coding Bootcamp</dt>
-          <dd>July 2022-October 2022</dd>
-        </dl>
-        <br />
-      </div>
-      <div id="experience">
-        <dl>
-          <h1>Experience</h1>
-          <dt>Assistant Manager</dt>
-          <dd>Jersey Mike's Subs</dd>
-          <dd>2020-2021</dd>
-          <dd>
-            ● Dedicated time to take stock and order trucks as needed to keep up
-            with dynamic sales
-          </dd>
-          <dd>
-            ● Remained motivated during shifts and created a positive
-            environment for staff
-          </dd>
-          <dd>
-            ● Ensured that shifts went smoothly and everyone stayed on task
-          </dd>
-          <br />
-          <dt>Shift Lead</dt>
-          <dd>Jersey Mike's Subs</dd>
-          <dd>2020-2021</dd>
-          <dd>
-            ● Dedicated time to take stock and order trucks as needed to keep up
-            with dynamic sales
-          </dd>
-          <dd>
-            ● Remained motivated during shifts and created a positive
-            environment for staff
-          </dd>
-          <dd>
-            ● Ensured that shifts went smoothly and everyone stayed on task
-          </dd>
-          <br />
-          <dt>Crew Member</dt>
-          <dd>Jersey Mike's Subs</dd>
-          <dd>2019-2020</dd>
-          <dd>
-            ● Dedicated time to take stock and order trucks as needed to keep up
-            with dynamic sales
-          </dd>
-          <dd>
-            ● Remained motivated during shifts and created a positive
-            environment for staff
-          </dd>
-          <dd>
-            ● Ensured that shifts went smoothly and everyone stayed on task
-          </dd>
-        </dl>
-      </div>
-      <div id="skills">
-      <Table striped bordered hover className="table">
-      <thead>
-        <tr>
-          <th>Front-end</th>
-          <th>Back-end</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>HTML</td>
-          <td>mySQL</td>
-        </tr>
-        <tr>
-          <td>CSS</td>
-          <td>Node.js</td>
-        </tr>
-      </tbody>
-    </Table>
-      </div>
+      <Row>
+        <Col>
+          <div id="education">
+            <dl>
+              <h1>Education</h1>
+              <dt>Rochester High School</dt>
+              <dd>2016-2020</dd>
+              <br />
+              <dt>Lincoln Land Community College</dt>
+              <dd>2018-2021</dd>
+              <br />
+              <dt>University of Denver Coding Bootcamp</dt>
+              <dd>July 2022-October 2022</dd>
+            </dl>
+            <br />
+          </div>
+          <div id="skills">
+            <Table striped bordered hover className="table">
+              <thead>
+                <tr>
+                  <th>Front-end</th>
+                  <th>Back-end</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>HTML</td>
+                  <td>mySQL</td>
+                </tr>
+                <tr>
+                  <td>CSS</td>
+                  <td>Node.js</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        </Col>
+        <Col xs={1}>
+          <div className="horizontal"></div>
+        </Col>
+        <Col>
+          <div id="experience">
+            <dl>
+              <h1>Experience</h1>
+              <dt>Assistant Manager</dt>
+              <dd>Jersey Mike's Subs</dd>
+              <dd>2020-2021</dd>
+              <dd>
+                ● Dedicated time to take stock and order trucks as needed to
+                keep up with dynamic sales
+              </dd>
+              <dd>
+                ● Remained motivated during shifts and created a positive
+                environment for staff
+              </dd>
+              <dd>
+                ● Ensured that shifts went smoothly and everyone stayed on task
+              </dd>
+              <br />
+              <dt>Shift Lead</dt>
+              <dd>Jersey Mike's Subs</dd>
+              <dd>2020-2021</dd>
+              <dd>
+                ● Dedicated time to take stock and order trucks as needed to
+                keep up with dynamic sales
+              </dd>
+              <dd>
+                ● Remained motivated during shifts and created a positive
+                environment for staff
+              </dd>
+              <dd>
+                ● Ensured that shifts went smoothly and everyone stayed on task
+              </dd>
+              <br />
+              <dt>Crew Member</dt>
+              <dd>Jersey Mike's Subs</dd>
+              <dd>2019-2020</dd>
+              <dd>
+                ● Dedicated time to take stock and order trucks as needed to
+                keep up with dynamic sales
+              </dd>
+              <dd>
+                ● Remained motivated during shifts and created a positive
+                environment for staff
+              </dd>
+              <dd>
+                ● Ensured that shifts went smoothly and everyone stayed on task
+              </dd>
+            </dl>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
