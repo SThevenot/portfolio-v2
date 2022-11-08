39b86b7e3392af7c0fb4e949fd9f943e96c4e704
SThevenot <sktmh007@gmail.com>
2022-11-08 09:26:06 -0600

    added tooltips and reformatted icons on home page

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -30,59 +30,120 @@ export default function Home() {
         />
       </div>
       <div id="skillIconsDiv">
+      <button>
+        <a className="nameHover" href="https://graphql.org/" target="_blank">
+          <span className="nameText">React.js</span>
         <img
           src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/reactjs.svg"
           alt="React.js"
         ></img>
+        </a>
+        </button>
+        <button>
+        <a className="nameHover" href="https://graphql.org/" target="_blank">
+          <span className="nameText">Javascript</span>
         <img
           src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/javascript.svg"
           alt="Javascript"
         ></img>
+        </a>
+        </button>
+        <button>
+        <a className="nameHover" href="https://graphql.org/" target="_blank">
+          <span className="nameText">CSS</span>
         <img
           src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/css.svg"
           alt="CSS"
         ></img>
+        </a>
+        </button>
+        <button>
+        <a className="nameHover" href="https://graphql.org/" target="_blank">
+          <span className="nameText">HTML</span>
         <img
           src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/html.svg"
           alt="HTML"
         ></img>
+        </a>
+        </button>
+        <button>
+        <a className="nameHover" href="https://graphql.org/" target="_blank">
+          <span className="nameText">Node.js</span>
         <img
           src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/nodejs.svg"
           alt="Node.js"
         ></img>
+        </a>
+        </button>
+        <button>
+        <a className="nameHover" href="https://graphql.org/" target="_blank">
+          <span className="nameText">Express.js</span>
         <img
           src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/express.svg"
           alt="Express.js"
         ></img>
-        <img
-          src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/graphql.svg"
-          alt="GraphQL"
-        ></img>
+        </a>
+        </button>
+        <button>
+        <a className="nameHover" href="https://graphql.org/" target="_blank">
+          <span className="nameText">GraphQL</span>
+          <img
+            src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/graphql.svg"
+            alt="GraphQL"
+          ></img>
+        </a>
+        </button>
+        <button>
+        <a className="nameHover" href="https://graphql.org/" target="_blank">
+          <span className="nameText">Jest</span>
         <img
           src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/jest.svg"
           alt="Jest"
         ></img>
+        </a>
+        </button>
+        <button>
+        <a className="nameHover" href="https://graphql.org/" target="_blank">
+          <span className="nameText">MongoDB</span>
         <img
           src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/mongodb.svg"
           alt="MongoDB"
         ></img>
-                <img
+        </a>
+        </button>
+        <button>
+        <a className="nameHover" href="https://graphql.org/" target="_blank">
+          <span className="nameText">mySQL</span>
+        <img
           src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/mysql.svg"
           alt="mySQL"
         ></img>
-                <img
+        </a>
+        </button>
+        <button>
+        <a className="nameHover" href="https://graphql.org/" target="_blank">
+          <span className="nameText">Bootstrap</span>
+        <img
           src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/bootstrap.svg"
           alt="Bootstrap"
         ></img>
-                <img
+        </a>
+        </button>
+        <button>
+        <a className="nameHover" href="https://graphql.org/" target="_blank">
+          <span className="nameText">npm</span>
+        <img
           src="https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/npm.svg"
           alt="npm"
         ></img>
+        </a>
+        </button>
       </div>
       <div id="aboutMeText">
-          <h3>
-            My name is Sarah Thevenot and I am a full-stack developer. I am determined to learn and grow as I continue my coding journey.
-          </h3>
+        <h3>
+          My name is Sarah Thevenot and I am a full-stack developer. I am
+          determined to learn and grow as I continue my coding journey.
+        </h3>
       </div>
     </div>
   );
