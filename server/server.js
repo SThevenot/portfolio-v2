ade516ba77f08cbec19f147da6763163593d22a9
SThevenot <sktmh007@gmail.com>
2022-11-17 08:44:12 -0600

    began adding backend and server.js to app

---
+++ server/server.js
@@ -0,0 +1,32 @@
+/** @format */
+
+const express = require("express");
+const path = require("path");
+
+const { ApolloServer } = require("apollo-server-express");
+
+const db = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const server = new ApolloServer({
+  typeDefs,
+});
+
+const startApolloServer = async (typeDefs) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Server running on port ${PORT}`);
+      console.log(`GraphQL at: http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+};
+
+startApolloServer(typeDefs);
