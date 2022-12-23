c6c4e8271cc0c54d432f285dbda08474444e40ee
SThevenot <sktmh007@gmail.com>
2022-12-23 15:13:51 -0600

    fixed connection issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 /** @format */
 const mongoose = require('mongoose');
 
+const uri = process.env.MONGODB_URI;
+
 mongoose.connect(
-  process.env.MONGODB_URI,
+  uri,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
