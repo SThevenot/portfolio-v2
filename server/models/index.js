588d3beee3d819c86b8930c400d38255dfd6d76e
SThevenot <sktmh007@gmail.com>
2023-01-04 10:58:45 -0600

    added resolvers for finding comments

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,6 @@
 
 const Project = require("./Project");
 const Skill = require("./Skills");
+const Comment = require("./Comment");
 
-module.exports = { Project, Skill };
+module.exports = { Project, Skill, Comment };
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,18 @@
 /** @format */
 
-const { Project, Skill } = require("../models");
+const { Project, Skill, Comment } = require("../models");
 
 const resolvers = {
   Query: {
     projects: async () => {
-      return Project.find();
+      return Project.find().populate('comments');
+    },
+    project: async (parent, { projectName }) => {
+      return User.findOne({ projectName }).populate('thoughts');
+    },
+    comments: async (parent, { projectName }) => {
+      const params = projectName ? { projectName } : {};
+      return Comment.find(params).sort({ createdAt: -1 });
     },
     skills: async () => {
       return Skill.find();
